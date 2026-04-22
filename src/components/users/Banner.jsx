c8817dc0799b184a7eb6a6507042a0bd21e0b5e1
AppconnectSolutions <Hr@appconnectsolutions.com>
2026-04-22 17:40:08 +0530

    banner Update

--- src/components/users/Banner.jsx
+++ src/components/users/Banner.jsx
@@ -5,39 +5,40 @@ import "./Banner.css";
 
 export default function Banner() {
 
-  const slides = [
-    {
-      img: "/assets/images/update_bg_1.jpeg",
-      title: "Healthy Living Starts",
-      sub: "Nature’s Freshness Delivered to Your Home",
-      pos: { top: "20%", left: "7%", textAlign: "left", width: "45%" }
-    },
-    {
-      img: "/assets/images/update_bg_2.jpeg",
-      title: "Instant Lemon Refreshment",
-      sub: "Beat the Heat with Vitalimes Lemon Powder",
-      pos: { top: "22%", right: "7%", textAlign: "right", width: "40%" }
-    },
-    {
-      img: "/assets/images/update_3.png",
-      title: "Pure Relief in Every Drop",
-      sub: "Natural Breathing Support for Your Family",
-      pos: { top: "15%", right: "6%", textAlign: "right", width: "40%" }
-    },
-    {
-      img: "/assets/images/update_bg_4.jpeg",
-      title: "Sip the Sun. Taste the Zest.",
-      sub: "Cool Down with Pure Lemon Bliss",
-      pos: { top: "28%", right: "6%", textAlign: "right", width: "40%" }
-    },
-    {
-      img: "/assets/images/bg_5.jpeg",
-      title: "Care for Your Heart Naturally",
-      sub: "Daily Wellness with Black Lemon Powder",
-      pos: { top: "22%", right: "6%", textAlign: "right", width: "40%" }
-    },
-  ];
+  const BASE_URL = "https://minio-console.appconnect.cloud/vitalimes-images/uploads";
 
+const slides = [
+  {
+    img: `${BASE_URL}/update_bg_1.jpeg`,
+    title: "Healthy Living Starts",
+    sub: "Nature’s Freshness Delivered to Your Home",
+    pos: { top: "20%", left: "7%", textAlign: "left", width: "45%" }
+  },
+  {
+    img: `${BASE_URL}/update_bg_2.jpeg`,
+    title: "Instant Lemon Refreshment",
+    sub: "Beat the Heat with Vitalimes Lemon Powder",
+    pos: { top: "22%", right: "7%", textAlign: "right", width: "40%" }
+  },
+  {
+    img: `${BASE_URL}/update_3.png`,
+    title: "Pure Relief in Every Drop",
+    sub: "Natural Breathing Support for Your Family",
+    pos: { top: "15%", right: "6%", textAlign: "right", width: "40%" }
+  },
+  {
+    img: `${BASE_URL}/update_bg_4.jpeg`,
+    title: "Sip the Sun. Taste the Zest.",
+    sub: "Cool Down with Pure Lemon Bliss",
+    pos: { top: "28%", right: "6%", textAlign: "right", width: "40%" }
+  },
+  {
+    img: `${BASE_URL}/bg_5.jpeg`,
+    title: "Care for Your Heart Naturally",
+    sub: "Daily Wellness with Black Lemon Powder",
+    pos: { top: "22%", right: "6%", textAlign: "right", width: "40%" }
+  },
+];
   return (
     <main>
       <Carousel fade interval={4000} controls indicators>
